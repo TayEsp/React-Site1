1d65ee254c6c285220c26ac3d538ef8b6a7b4e2e
TayEsp <tayna.respinosa@gmail.com>
2023-05-07 20:09:20 -0300

    Arrumando a pagina imagens e lista

--- src/components/pages/Imagens.js
+++ src/components/pages/Imagens.js
@@ -5,13 +5,14 @@ import imagem2 from '../../galeria/imagem2.jpg'
 import imagem3 from '../../galeria/imagem3.jpg'
 import imagem4 from '../../galeria/imagem4.jpg'
 
-import { useState , useEffect} from 'react'
+import { useEffect, useState } from 'react'
 
 function Imagens(props){
 
     const navigate = useNavigate()
-    const[imagem,setImagem] = useState()
-    const[galeria,setGaleria] = useState()
+    const[imagem,setImagem] = useState([])
+    const[galeria,setGaleria] = useState([])
+    var id = 0;
     
     let imagens = {
         imagem1,
@@ -21,30 +22,39 @@ function Imagens(props){
     }
 
     useEffect(()=>{
-        fetch("http://localhost:5000/imagens",{method:"GET",headers:{"Content-Type":"application/json"}})
+        fetch("http://localhost:5000/galeria",{method:"GET",headers:{"Content-Type":"application/json"}})
         .then((resp)=>resp.json())
-        .then((data)=>{setImagem(data)})
+        .then((data)=>{setImagem(data)
+            console.log(data)})
         .catch((err)=>console.log(err))
     },[])
 
-
-    function createPost(imagens) {
-        fetch("http://localhost:5000/imagens",{method:"POST",headers:{"Content-Type":"application/json"},
-        body: JSON.stringify(imagens)})
+    function createPost(galeria) {
+        fetch("http://localhost:5000/galeria",{method:"POST",headers:{"Content-Type":"application/json"},
+        body: JSON.stringify(galeria)})
         .then((resp)=>resp.json())
-        .then((data)=>{console.log })
+        .then((data)=>{console.log(data)
+            document.location.reload(false)})
         .catch((err)=>console.log(err))
+    }
+
+    function removeProject(id){
+
+        fetch(`http://localhost:5000/galeria/${trocarImagem(id)}`,{method:'DELETE',headers:{"Content-Type":"application/json"}})
+        .then(resp => resp.json())
+        .then(data => {setGaleria(galeria.filter((galeria)=> galeria.name !== trocarImagem(id)))
+            document.location.reload(false)})
     }
 
     function trocarImagem(num){
         if (num == 1){
-            setImagem("imagem1")
+            return("imagem1")
         } else if(num == 2){
-            setImagem("imagem2")
+            return("imagem2")
         } else if(num == 3){
-            setImagem("imagem3")
+            return("imagem3")
         } else if(num == 4){
-            setImagem("imagem4")
+            return("imagem4")
         }
     }
 
@@ -53,19 +63,27 @@ function Imagens(props){
         createPost(galeria)
     }
 
+    const remove = (e) => {
+        e.preventDefault()
+        removeProject(id)
+    }
+
+    function handleChange(e){
+        setGaleria({...galeria, name: (trocarImagem(e.target.value))}, {[e.target.key]: (id=id+1)})
+    }
+
     return(
         <div>
             <h1>{props.name}</h1>
             <p>Galeria de Imagens</p>
             <form onSubmit={submit}>
-                <input type="number" placeholder="Insira o número da imagem:" handleOnChange={createPost}/>
-                <button type="submit">inserir</button>
-                <button type="submit">excluir</button>
+                <input type="text" placeholder="Insira o número da imagem:" onChange={handleChange} />
+                <button type="submit">inserir</button>  
+                <button type="submit" onClick={remove}>excluir</button>
             </form>
-            {galeria.map((galeria)=>(
-                        <img src={imagens[galeria.src]} key={galeria.id} style={{width:'20em'}} alt="imagens"/>
+            {imagem.map((imagem)=>(
+                        <img src={imagens[imagem.name]} key = {imagem.key} style={{width:'20em'}} alt="imagens"/>
             ))}
-            <img src={imagens[imagem]} style={{width:'20em'}} alt="imagens"/>
         </div>
     )
 }
